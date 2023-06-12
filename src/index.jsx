51403afc8f0c2a61a84519010ee7eccfcb735f95
Sambit Nayak <Sambit Kumar Nayak>
2023-06-12 17:39:26 +0530

    Added all files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from '../src/app';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
